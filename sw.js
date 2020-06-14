d216835551edb71649bdf6800f15768389dc2c6f
winlian99 <62795717+winlian99@users.noreply.github.com>
2020-06-14 16:36:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,11 +4,18 @@ self.addEventListener('install', function(event) {
         .then(function(cache) {
           cache.addAll([
             '/',
-            '/favicon.ico',
-            '/manifest.json',
             '/index.html',
             '/src/css/app.css',
-            '/src/js/app.js'
+            '/src/js/app.js',
+            '/manifest.json',
+            'src/images/icons/icon-72x72.png',
+            'src/images/icons/icon-86x86.png',
+            'src/images/icons/icon-128x128.png',
+            'src/images/icons/icon-144x144.png',
+            'src/images/icons/icon-152x152.png',
+            'src/images/icons/icon-192x192.png',
+            'src/images/icons/icon-384x384.png',
+            'src/images/icons/icon-512x512.png',
           ])
         })
     );
@@ -23,4 +30,3 @@ self.addEventListener('install', function(event) {
         })
     );
   });
-    
