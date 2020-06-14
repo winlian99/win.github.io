2369aeb70f7231dd09f60705c9ffce7eae5093b7
winlian99 <62795717+winlian99@users.noreply.github.com>
2020-06-14 15:10:02 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', function(event) {
         .then(function(cache) {
           cache.addAll([
             '/',
+            '/favicon.ico',
             '/index.html',
             '/src/css/app.css',
             '/src/js/app.js'
