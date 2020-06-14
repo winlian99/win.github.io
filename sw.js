9cd21d9228aae1cae250b4194f0c7c3a9fd9be7b
winlian99 <62795717+winlian99@users.noreply.github.com>
2020-06-14 16:54:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,14 +8,14 @@ self.addEventListener('install', function(event) {
             '/src/css/app.css',
             '/src/js/app.js',
             '/manifest.json',
-            'src/images/icons/icon-72x72.png',
-            'src/images/icons/icon-86x86.png',
-            'src/images/icons/icon-128x128.png',
-            'src/images/icons/icon-144x144.png',
-            'src/images/icons/icon-152x152.png',
-            'src/images/icons/icon-192x192.png',
-            'src/images/icons/icon-384x384.png',
-            'src/images/icons/icon-512x512.png',
+            '/src/images/icons/icon-72x72.png',
+            '/src/images/icons/icon-86x86.png',
+            '/src/images/icons/icon-128x128.png',
+            '/src/images/icons/icon-144x144.png',
+            '/src/images/icons/icon-152x152.png',
+            '/src/images/icons/icon-192x192.png',
+            '/src/images/icons/icon-384x384.png',
+            '/src/images/icons/icon-512x512.png',
           ])
         })
     );
