1607151397c79bf2d1d49aefb22a31922127173e
winlian99 <62795717+winlian99@users.noreply.github.com>
2020-06-14 17:23:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('install', function(event) {
             '/src/js/app.js',
             '/manifest.json',
             '/src/images/icons/icon-72x72.png',
-            '/src/images/icons/icon-86x86.png',
+            '/src/images/icons/icon-96x96.png',
             '/src/images/icons/icon-128x128.png',
             '/src/images/icons/icon-144x144.png',
             '/src/images/icons/icon-152x152.png',
